96edae77a8b7da53b34e69b46bad34ef6bde9802
Guillaume Marty <edo999@gmail.com>
2015-04-15 16:16:46 +0100

    Plot the measures on a scattergraph

--- js/app.js
+++ js/app.js
@@ -21,10 +21,12 @@ require([
     elements: {
       reload: document.getElementById('reload'),
       table: document.getElementById('table'),
-      chart: document.getElementById('chart')
+      barchart: document.getElementById('barchart'),
+      scatterplot: document.getElementById('scatterplot')
     },
 
     firstRun: true,
+    graph: {},
 
     init: function() {
       this.rawWorker.postMessage(0);
@@ -44,7 +46,8 @@ require([
 
     startMeasuring: function() {
       this.elements.table.innerHTML = '';
-      this.initPlot();
+      this.initBarChart();
+      this.initScatterPlot();
 
       this.firstRun = true;
       this.measureLatencyOfThreads([]);
@@ -211,48 +214,55 @@ require([
         {name: '90%', value: roundtrip90Percentile},
         {name: '95%', value: roundtrip95Percentile}
       ], shortTitle);
+
+      this.plotScatter(values.map((value, index) => {
+        return {
+          name: shortTitle,
+          value: value[2],
+          id: index
+        };
+      }));
     },
 
     // Graph related methods.
-    initPlot: function() {
-      this.elements.chart.innerHTML = '';
+    initBarChart: function() {
+      this.elements.barchart.innerHTML = '';
 
-      this.graph = {};
-      this.graph.margin = {top: 5, right: 45, bottom: 30, left: 30};
-      this.graph.width = 320 - this.graph.margin.left - this.graph.margin.right;
-      this.graph.height = 240 - this.graph.margin.top - this.graph.margin.bottom;
+      this.graph.g = {};
+      this.graph.g.margin = {top: 5, right: 45, bottom: 30, left: 30};
+      this.graph.g.width = 320 - this.graph.g.margin.left - this.graph.g.margin.right;
+      this.graph.g.height = 240 - this.graph.g.margin.top - this.graph.g.margin.bottom;
 
-      this.graph.x = d3.scale.ordinal()
-        .rangeRoundBands([0, this.graph.width], .1);
+      this.graph.g.x = d3.scale.ordinal()
+        .rangeRoundBands([0, this.graph.g.width], .1);
 
-      this.graph.y = d3.scale.linear()
-        .domain([0, this.graph.maxY])
-        .range([this.graph.height, 0]);
+      this.graph.g.y = d3.scale.linear()
+        .domain([0, this.graph.g.maxY])
+        .range([this.graph.g.height, 0]);
 
-      this.graph.xAxis = d3.svg.axis()
-        .scale(this.graph.x)
+      this.graph.g.xAxis = d3.svg.axis()
+        .scale(this.graph.g.x)
         .orient('bottom');
 
-      this.graph.yAxis = d3.svg.axis()
-        .scale(this.graph.y)
-        .orient('left')
-        .ticks(10);
+      this.graph.g.yAxis = d3.svg.axis()
+        .scale(this.graph.g.y)
+        .orient('left');
 
-      this.graph.color = d3.scale.category10();
+      this.graph.g.color = d3.scale.category10();
 
-      this.graph.chart = d3.select('#chart').append('svg')
-        .attr('width', this.graph.width + this.graph.margin.left + this.graph.margin.right)
-        .attr('height', this.graph.height + this.graph.margin.top + this.graph.margin.bottom).append('g')
-        .attr('transform', `translate(${this.graph.margin.left},${this.graph.margin.top})`);
+      this.graph.g.chart = d3.select('#barchart').append('svg')
+        .attr('width', this.graph.g.width + this.graph.g.margin.left + this.graph.g.margin.right)
+        .attr('height', this.graph.g.height + this.graph.g.margin.top + this.graph.g.margin.bottom).append('g')
+        .attr('transform', `translate(${this.graph.g.margin.left},${this.graph.g.margin.top})`);
 
-      this.graph.chart.xAxisEl = this.graph.chart.append('g')
+      this.graph.g.xAxisEl = this.graph.g.chart.append('g')
         .attr('class', 'x axis')
-        .attr('transform', `translate(0,${this.graph.height})`);
+        .attr('transform', `translate(0,${this.graph.g.height})`);
 
-      this.graph.chart.yAxisEl = this.graph.chart.append('g')
+      this.graph.g.yAxisEl = this.graph.g.chart.append('g')
         .attr('class', 'y axis');
 
-      this.graph.chart.yAxisEl
+      this.graph.g.yAxisEl
         .append('text')
         .attr('transform', 'rotate(-90)')
         .attr('y', 6)
@@ -260,19 +270,19 @@ require([
         .style('text-anchor', 'end')
         .text('Latency in ms');
 
-      this.graph.maxY = 0;
-      this.graph.set = 0;
-      this.graph.data = [];
+      this.graph.g.maxY = 0;
+      this.graph.g.set = 0;
+      this.graph.g.data = [];
     },
 
     plotBarChart: function(data, title) {
-      if (this.graph.set === 1) {
-        this.graph.x.domain(data.map(d => d.name));
+      if (this.graph.g.set === 1) {
+        this.graph.g.x.domain(data.map(d => d.name));
 
         // Captions for the different statistic functions plotted.
-        var caption = this.graph.chart.append('g')
+        var caption = this.graph.g.chart.append('g')
           .attr('class', 'caption')
-          .attr('transform', `translate(${this.graph.margin.right},0)`);
+          .attr('transform', `translate(${this.graph.g.margin.right},0)`);
 
         var legend = caption.selectAll('.legend')
           .data(data)
@@ -281,52 +291,157 @@ require([
           .attr('transform', (d, i) => `translate(0,${(i * 20)})`);
 
         legend.append('rect')
-          .attr('x', this.graph.width - 18)
+          .attr('x', this.graph.g.width - 18)
           .attr('width', 18)
           .attr('height', 18)
-          .style('fill', d => this.graph.color(d.name));
+          .style('fill', d => this.graph.g.color(d.name));
 
         legend.append('text')
-          .attr('x', this.graph.width - 24)
+          .attr('x', this.graph.g.width - 24)
           .attr('y', 9)
           .attr('dy', '.35em')
           .style('text-anchor', 'end')
           .text(d => d.name);
       }
 
-      data.forEach(item => item.name += ` ${this.graph.set} `);
+      data.forEach(item => item.name += ` ${this.graph.g.set} `);
 
-      this.graph.data = this.graph.data.concat(data);
+      this.graph.g.data = this.graph.g.data.concat(data);
 
       // Caption for each measured set.
-      this.graph.chart.xAxisEl
+      this.graph.g.xAxisEl
         .append('text')
-        .attr('transform', `translate(${(this.graph.set * (this.graph.width) / 3)},12)`)
+        .attr('transform', `translate(${(this.graph.g.set * this.graph.g.width / 3)},12)`)
         .attr('y', 6)
         .attr('dy', '.71em')
         .text(` ${title} `);
 
-      this.graph.set++;
+      this.graph.g.set++;
 
-      if (this.graph.set >= 3) {
-        this.graph.maxY = d3.max(this.graph.data, d => d.value);
-
-        this.graph.x.domain(this.graph.data.map(d => d.name));
-        this.graph.y.domain([0, this.graph.maxY]);
-
-        this.graph.chart.xAxisEl.call(this.graph.xAxis);
-        this.graph.chart.yAxisEl.call(this.graph.yAxis);
-
-        this.graph.chart.selectAll('.bar')
-          .data(this.graph.data)
-          .enter().append('rect')
-          .attr('class', 'bar')
-          .attr('x', d => this.graph.x(d.name))
-          .attr('y', d => this.graph.y(d.value))
-          .attr('height', d => this.graph.height - this.graph.y(d.value))
-          .attr('width', this.graph.x.rangeBand())
-          .style('fill', d => this.graph.color(d.name.split(' ')[0]));
+      if (this.graph.g.set < 3) {
+        return;
       }
+
+      this.graph.g.maxY = d3.max(this.graph.g.data, d => d.value);
+
+      this.graph.g.x.domain(this.graph.g.data.map(d => d.name));
+      this.graph.g.y.domain([0, this.graph.g.maxY]);
+
+      this.graph.g.xAxisEl.call(this.graph.g.xAxis);
+      this.graph.g.yAxisEl.call(this.graph.g.yAxis);
+
+      this.graph.g.chart.selectAll('.bar')
+        .data(this.graph.g.data)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', d => this.graph.g.x(d.name))
+        .attr('y', d => this.graph.g.y(d.value))
+        .attr('height', d => this.graph.g.height - this.graph.g.y(d.value))
+        .attr('width', this.graph.g.x.rangeBand())
+        .style('fill', d => this.graph.g.color(d.name.split(' ')[0]));
+    },
+
+    initScatterPlot: function() {
+      this.elements.scatterplot.innerHTML = '';
+
+      this.graph.s = {};
+      this.graph.s.margin = {top: 5, right: 5, bottom: 30, left: 30};
+      this.graph.s.width = 320 - this.graph.s.margin.left - this.graph.s.margin.right;
+      this.graph.s.height = 240 - this.graph.s.margin.top - this.graph.s.margin.bottom;
+
+      this.graph.s.x = d3.scale.linear()
+        .domain([0, ITERATIONS])
+        .range([0, this.graph.s.width]);
+
+      this.graph.s.y = d3.scale.linear()
+        .domain([0, 1])
+        .range([this.graph.s.height, 0]);
+
+      this.graph.s.xAxis = d3.svg.axis()
+        .scale(this.graph.s.x)
+        .orient('bottom');
+
+      this.graph.s.yAxis = d3.svg.axis()
+        .scale(this.graph.s.y)
+        .orient('left');
+
+      this.graph.s.color = d3.scale.category10();
+
+      this.graph.s.chart = d3.select('#scatterplot').append('svg')
+        .attr('width', this.graph.s.width + this.graph.s.margin.left + this.graph.s.margin.right)
+        .attr('height', this.graph.s.height + this.graph.s.margin.top + this.graph.s.margin.bottom).append('g')
+        .attr('transform', `translate(${this.graph.s.margin.left},${this.graph.s.margin.top})`);
+
+      this.graph.s.xAxisEl = this.graph.s.chart.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', `translate(0,${this.graph.s.height})`);
+
+      this.graph.s.yAxisEl = this.graph.s.chart.append('g')
+        .attr('class', 'y axis');
+
+      this.graph.s.xAxisEl
+        .append('text')
+        .attr('x', this.graph.s.width)
+        .attr('y', 0)
+        .style('text-anchor', 'end')
+        .text('Measure');
+
+      this.graph.s.yAxisEl
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 6)
+        .attr('dy', '.71em')
+        .style('text-anchor', 'end')
+        .text('Latency in ms');
+
+      this.graph.s.maxY = 0;
+      this.graph.s.set = 0;
+      this.graph.s.data = [];
+    },
+
+    plotScatter: function(data) {
+      this.graph.s.data = this.graph.s.data.concat(data);
+
+      this.graph.s.set++;
+
+      if (this.graph.s.set < 3) {
+        return;
+      }
+
+      this.graph.s.maxY = d3.max(this.graph.s.data, d => d.value);
+
+      this.graph.s.y.domain([0, this.graph.s.maxY]);
+
+      this.graph.s.xAxisEl.call(this.graph.s.xAxis);
+      this.graph.s.yAxisEl.call(this.graph.s.yAxis);
+
+      this.graph.s.chart.selectAll('.dot')
+        .data(this.graph.s.data)
+        .enter().append('circle')
+        .attr('class', 'dot')
+        .attr('r', 2)
+        .attr('cx', d => this.graph.s.x(d.id))
+        .attr('cy', d => this.graph.s.y(d.value))
+        .style('fill', d => this.graph.s.color(d.name));
+
+      var legend = this.graph.s.chart.selectAll('.legend')
+        .data(this.graph.s.color.domain())
+        .enter().append('g')
+        .attr('class', 'legend')
+        .attr('transform', (d, i) => `translate(0,${(i * 20)})`);
+
+      legend.append('rect')
+        .attr('x', this.graph.s.width - 18)
+        .attr('width', 18)
+        .attr('height', 18)
+        .style('fill', this.graph.s.color);
+
+      legend.append('text')
+        .attr('x', this.graph.s.width - 24)
+        .attr('y', 9)
+        .attr('dy', '.35em')
+        .style('text-anchor', 'end')
+        .text(d => d);
     }
   };
 
